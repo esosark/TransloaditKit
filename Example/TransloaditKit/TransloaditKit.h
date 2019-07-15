7de2db4b81eacb42a202b027efc242d854a5c1c3
Mark R. Masterson <mark@masterson.io>
2019-07-15 10:03:18 -0700

    Catrthage

---
+++ Example/TransloaditKit/TransloaditKit.h
@@ -0,0 +1,19 @@
+//
+//  TransloaditKit.h
+//  TransloaditKit
+//
+//  Created by Mark Masterson on 7/15/19.
+//  Copyright © 2019 Mark R. Masterson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TransloaditKit.
+FOUNDATION_EXPORT double TransloaditKitVersionNumber;
+
+//! Project version string for TransloaditKit.
+FOUNDATION_EXPORT const unsigned char TransloaditKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TransloaditKit/PublicHeader.h>
+
+
